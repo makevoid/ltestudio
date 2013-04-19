fbb390d594fa3a477552fd8e92c1b67ed781c378
peppe <peppe.dimarzo@gmail.com>
2013-04-19 19:19:30 +0200

    pronto da pubblicare

--- config/shirts.rb
+++ config/shirts.rb
@@ -1,32 +1,28 @@
 DESIGNS = [
-  { name: "TWELVE MONK",      file: "12monk" },
-  { name: "BOM",              file: "bom" },
-  { name: "CLINT",            file: "clint" },
-  { name: "WOMAN",            file: "donna" },
-  { name: "IMPRONTA",         file: "impronta" },
-  { name: "LOVE",             file: "love" },
-  { name: "SATRI",            file: "satrial" },
-  { name: "SKULL",            file: "skull" },
+  { name: "control_mind",      file: "control_mind" },
+  { name: "angela",      file: "angela" },
+  { name: "reality_is_not_real",      file: "reality_is_not_real" },
+  { name: "breathe",      file: "breathe" },
+  { name: "doc",      file: "doc" },
+  { name: "bttf_almanac",      file: "bttf_almanac" },
+  { name: "spike",      file: "spike" },
+  { name: "b_lebowski",      file: "b_lebowski" },
+  { name: "identity",      file: "identity" },
+  { name: "jespin",      file: "jespin" },
+  { name: "twelve_monkeys",      file: "twelve_monkeys" },
 ]
 
 WORKS = [
-  { name: "SUBWAY",  file: "subway" },
-  { name: "DUDE",  file: "dude" },
-  { name: "HOMELESS#1",  file: "homeless_1" },
-  { name: "HOMELESS#2",  file: "homeless_2" },
-  { name: "WALTER WHITE",  file: "walter" },
+  { name: "walter_white",  file: "walter_white" },
+  { name: "homeless",  file: "homeless" },
+  { name: "homeless_b",  file: "homeless_b" },
+  { name: "in_subway",  file: "in_subway" },
 ]
 
 STAMPE = [
-  { name: "Plottaggi",  file: "plotter" },
-  { name: "Biglietti da visita",  file: "biglietti" },
-  { name: "Fotocopie",  file: "fotocopie" },
-  { name: "Brochure",  file: "brochure" },
-  { name: "Rilegature",  file: "rilegature" },
-  { name: "Adesivi",  file: "adesivi" },
-  { name: "Tesi di laurea",  file: "tesi" },
-  { name: "Cataloghi",  file: "cataloghi" },
-  { name: "Vetrofanie",  file: "vetrofanie" },
+  { name: "biglietti",  file: "biglietti" },
+  { name: "plottaggi",  file: "plottaggi" },  
+  
 ]
 
 include Mhash
